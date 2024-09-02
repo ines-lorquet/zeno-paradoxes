f8cd1e4c0d2406767b1151084c00f629e682ee4d
NovaStarmax <lapvpgames@gmail.com>
2024-09-02 19:25:32 +0200

    Début pb1

--- dichotomie.py
+++ dichotomie.py
@@ -1,4 +1,4 @@
-def distance(n):
+def dichotomie_antoine(n):
     caillou = 0
     arbre = 8
     diviseur = 2
@@ -6,7 +6,22 @@ def distance(n):
     for i in range (1, n) : 
         caillou = arbre / diviseur
         diviseur *= 2
-        print ("Voici la distance restante :" , caillou)
+        score = score + 1
+        print (score , "Voici la distance restante :" , caillou)
+
+# n = int(input("Entrez le nombre d’itération que vous voulez effectuer : "))
+# dichotomie_antoine(n)
+
+def tortue_antoine(n):
+    achille = 0
+    tortue = 4
+    temps = 0
+    distance = 0
+    for i in range (1, n) :
+        distance = (achille + tortue) / 2
+        achille = tortue
+        tortue = tortue + distance
+        print(f"Achille est à {achille} mètres et la Tortue est à {tortue} mètres.")
 
 n = int(input("Entrez le nombre d’itération que vous voulez effectuer : "))
-distance(n)
+tortue_antoine(n)
