c2de117c951692baf76b04a9e71d406fa7530b40
NovaStarmax <lapvpgames@gmail.com>
2024-09-02 17:56:15 +0200

    Solus Antoine

--- dichotomie.py
+++ dichotomie.py
@@ -1,4 +1,12 @@
-a = 8
-while a != 0 :
-    a = a / 2
-    print(a)
+def distance(n):
+    caillou = 0
+    arbre = 8
+    diviseur = 2
+    score = 0
+    for i in range (1, n) : 
+        caillou = arbre / diviseur
+        diviseur *= 2
+        print ("Voici la distance restante :" , caillou)
+
+n = int(input("Entrez le nombre d’itération que vous voulez effectuer : "))
+distance(n)
