ec42659615ae48b09d8667c332d2c53aae08933e
NovaStarmax <lapvpgames@gmail.com>
2024-09-03 11:33:16 +0200

    tortue antoine

--- dichotomie.py
+++ dichotomie.py
@@ -12,16 +12,22 @@ def dichotomie_antoine(n):
 # n = int(input("Entrez le nombre d’itération que vous voulez effectuer : "))
 # dichotomie_antoine(n)
 
-def tortue_antoine(n):
+def tortue_antoine_v0(n):
     achille = 0
     tortue = 4
     temps = 0
-    distance = 0
+    iteration = 2
+    ecart = 0
     for i in range (1, n) :
-        distance = (achille + tortue) / 2
+        iteration *= 2
+        print("itération : ", iteration)
+        ecart = (tortue - achille) / 2 ** iteration
+        print("écart : ", ecart)
         achille = tortue
-        tortue = tortue + distance
+        print("achille : ",achille)
+        tortue = achille + ecart
+        print("tortue : ",tortue)
         print(f"Achille est à {achille} mètres et la Tortue est à {tortue} mètres.")
 
 n = int(input("Entrez le nombre d’itération que vous voulez effectuer : "))
-tortue_antoine(n)
+tortue_antoine_v0(n)
