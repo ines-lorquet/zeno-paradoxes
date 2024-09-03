397efe89578c149941149d8575e51ad60d6d34c2
ines-lorquet <ines.lorquet@laplateforme.io>
2024-09-03 13:27:19 +0200

    [Modifier la fonction achille_tortue]

--- achille_1.py
+++ achille_1.py
@@ -1,22 +1,24 @@
-def display(pos_1,pos_2):
-        run = "_______________________________"
-        step = list(run)
-        step.insert(pos_1, "l")
-        step.insert(pos_2, "0")
-        print(''.join(step))
-          
-def achi_tort():
-    pos_a = 0 
-    pos_t = 5
+def display(pos_1, pos_2):
+    run = "____________________________"
+    step = list(run)
+    step.insert(pos_1, "1")
+    step.insert(pos_2, "0")
+    print("".join(step))
+
+
+def achille_tortue():
+    pos_a = 0
+    pos_t = 15
     vit_a = 2
     vit_t = 1
-    while pos_a < 30:
+
+    while pos_a < 28:
         A = int(pos_a)
         T = int(pos_t)
-        print(A,T)
-        display(A,T)
-        tp_a = (pos_a - pos_t) / vit_a
+        display(A, T)
+        tp_a = (pos_t - pos_a) / vit_a
         pos_a = pos_t
-        pos_t += pos_t + (vit_t * tp_a)
+        pos_t = pos_t + (vit_t * tp_a)
 
-achi_tort()
+
+achille_tortue()
