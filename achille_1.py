1023f94238573530a1b936933efe4860707cb4fe
ines-lorquet <ines.lorquet@laplateforme.io>
2024-09-03 13:47:10 +0200

    [Ajout des class Achille et Display_para]

--- achille_1.py
+++ achille_1.py
@@ -1,24 +1,24 @@
-def display(pos_1, pos_2):
-    run = "____________________________"
-    step = list(run)
-    step.insert(pos_1, "1")
-    step.insert(pos_2, "0")
-    print("".join(step))
+from display import Display_para
 
 
-def achille_tortue():
-    pos_a = 0
-    pos_t = 15
-    vit_a = 2
-    vit_t = 1
+class Achille(Display_para):
+    def __init__(self):
+        super().__init__()
 
-    while pos_a < 28:
-        A = int(pos_a)
-        T = int(pos_t)
-        display(A, T)
-        tp_a = (pos_t - pos_a) / vit_a
-        pos_a = pos_t
-        pos_t = pos_t + (vit_t * tp_a)
+    def achille_tortue(self):
+        pos_a = 0
+        pos_t = 15
+        vit_a = 2
+        vit_t = 1
+
+        while pos_a < 28:
+            A = int(pos_a)
+            T = int(pos_t)
+            self.display_terminal(A, T)
+            tp_a = (pos_t - pos_a) / vit_a
+            pos_a = pos_t
+            pos_t = pos_t + (vit_t * tp_a)
 
 
-achille_tortue()
+test = Achille()
+test.achille_tortue()
