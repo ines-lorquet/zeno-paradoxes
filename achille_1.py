a10fc8ee90ae668287237937493b44a16eb4b24a
ines-lorquet <ines.lorquet@laplateforme.io>
2024-09-03 13:02:27 +0200

    [Ajout de la méthode d'affichage]

--- achille_1.py
+++ achille_1.py
@@ -1,12 +1,22 @@
-def achi_tort():
-    A, T = 0, 5
-    while A != 15:
-        A, T = T, T + T // 2
-        print(A, T)
-        run = "________________"
+def display(pos_1,pos_2):
+        run = "_______________________________"
         step = list(run)
-        step.insert(A, "A")
-        step.insert(T, "T")
+        step.insert(pos_1, "l")
+        step.insert(pos_2, "0")
         print(''.join(step))
+          
+def achi_tort():
+    pos_a = 0 
+    pos_t = 5
+    vit_a = 2
+    vit_t = 1
+    while pos_a < 30:
+        A = int(pos_a)
+        T = int(pos_t)
+        print(A,T)
+        display(A,T)
+        tp_a = (pos_a - pos_t) / vit_a
+        pos_a = pos_t
+        pos_t += pos_t + (vit_t * tp_a)
 
 achi_tort()
