bf203c5033c9b8b4656c3e47ed020400392efa1d
NovaStarmax <lapvpgames@gmail.com>
2024-09-04 11:18:00 +0200

    Fin display

--- display_tortue.py
+++ display_tortue.py
@@ -1,4 +1,4 @@
-from termcolor import colored
+from termcolor import colored, cprint
 import time
 import math
 
@@ -8,28 +8,44 @@ Vitesse_T = 1
 Position_A = 0
 Position_T = 15
 
+# Position initiale
+pos_ini_A = math.ceil(Position_A)
+pos_ini_T = math.ceil(Position_T)
+
 display = 156
 
-# Logique d’affichage graphique
+# Affichage de la position initiale avant le calcul
+liste = ['#'] * display
+liste[pos_ini_A] = colored('A', "red")
+liste[pos_ini_T] = colored('T', "green")
+print(''.join(liste))
+time.sleep(0.7)
 
-for i in range (0, 10):
-    temps_A = (Position_T-Position_A) / Vitesse_A
+# Logique d’affichage graphique
+for i in range(0, 6):
+    temps_A = (Position_T - Position_A) / Vitesse_A
     Position_A = Position_T
     Position_T = Position_T + (Vitesse_T * temps_A)
 
     # Il faut arrondir les calculs pour permettre l’affichage graphique
-
     pos_A = math.ceil(Position_A)
     pos_T = math.ceil(Position_T)
 
     liste = ['#'] * display
 
-    if Position_A==Position_T :
-        liste[pos_A] = colored('X', 'yellow')
-    else : 
-        liste[pos_A] = colored('A', "red")
-        liste[pos_T] = colored('T', "green")
+    if pos_A == pos_T:
+        liste[pos_A] = colored('X', 'yellow')  
+    else:
+        liste[pos_A] = colored('A', "red")  
+        liste[pos_T] = colored('T', "green")  
+
     print(''.join(liste))
     time.sleep(0.5)
 
-print("fin de la simulation")
+print("-" * 156)
+cprint("X", "yellow", end="")
+print(" Signifie que la valeur entre la Tortue et Achille est extrèmement petite.")
+cprint("Achille ne parvient jamais à dépasser la Tortue …", "magenta")
+print("-" * 156)
+cprint("Fin de la simulation", "red")
+print("-" * 156)
