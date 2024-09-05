2ae2e0e6000db526c0cb9c7ad6dec05f9be7aa5f
NovaStarmax <lapvpgames@gmail.com>
2024-09-05 12:05:25 +0200

    fin de branche

---
+++ dichotomie_pygame.py
@@ -0,0 +1,59 @@
+import pygame
+
+def dichotomie_antoine(n):
+    caillou = 0
+    arbre = 8
+    diviseur = 2
+    score = 0
+    for i in range (1, n) : 
+        caillou = arbre / diviseur
+        diviseur *= 2
+        score = score + 1
+        print (score , "Voici la distance restante :" , caillou)
+
+n = int(input("Entrez le nombre d’itération que vous voulez effectuer : "))
+dichotomie_antoine(n)
+
+# Example file showing a circle moving on screen
+
+
+# pygame setup
+pygame.init()
+screen = pygame.display.set_mode((1280, 720))
+clock = pygame.time.Clock()
+running = True
+dt = 0
+
+player_pos = pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2)
+
+while running:
+    # poll for events
+    # pygame.QUIT event means the user clicked X to close your window
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    # fill the screen with a color to wipe away anything from last frame
+    screen.fill("purple")
+
+    pygame.draw.circle(screen, "red", player_pos, 40)
+
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_w]:
+        player_pos.y -= 300 * dt
+    if keys[pygame.K_s]:
+        player_pos.y += 300 * dt
+    if keys[pygame.K_a]:
+        player_pos.x -= 300 * dt
+    if keys[pygame.K_d]:
+        player_pos.x += 300 * dt
+
+    # flip() the display to put your work on screen
+    pygame.display.flip()
+
+    # limits FPS to 60
+    # dt is delta time in seconds since last frame, used for framerate-
+    # independent physics.
+    dt = clock.tick(60) / 1000
+
+pygame.quit()
