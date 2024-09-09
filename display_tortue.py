63c4a735da17303916c3aafa9549d6ee13da37d0
NovaStarmax <lapvpgames@gmail.com>
2024-09-09 13:43:14 +0200

    traduction anglaise

--- display_tortue.py
+++ display_tortue.py
@@ -2,56 +2,56 @@ from termcolor import colored, cprint
 import time
 import math
 
-# Valeurs 
-Vitesse_A = 6
-Vitesse_T = 3
+# Values
+Speed_A = 6
+Speed_T = 3
 Position_A = 0
 Position_T = 40
 
-# Position initiale
-pos_ini_A = math.ceil(Position_A)
-pos_ini_T = math.ceil(Position_T)
+# Initial position
+initial_pos_A = math.ceil(Position_A)
+initial_pos_T = math.ceil(Position_T)
 
 display = 156
 
-# Affichage de la position initiale avant le calcul
-liste = ['.'] * display
-liste[pos_ini_A] = colored('A', "red")
-liste[pos_ini_T] = colored('T', "green")
-print(''.join(liste))
+# Display the initial position before the calculation
+display_list = ['.'] * display
+display_list[initial_pos_A] = colored('A', "red")
+display_list[initial_pos_T] = colored('T', "green")
+print(''.join(display_list))
 time.sleep(0.7)
 
-# Logique d’affichage graphique
+# Graphical display logic
 for i in range(0, 8):
-    temps_A = (Position_T - Position_A) / Vitesse_A
+    time_A = (Position_T - Position_A) / Speed_A
     Position_A = Position_T
-    Position_T = Position_T + (Vitesse_T * temps_A)
+    Position_T = Position_T + (Speed_T * time_A)
 
-    # Il faut arrondir les calculs pour permettre l’affichage graphique
+    # We need to round the calculations to allow graphical display
     pos_A = math.ceil(Position_A)
     pos_T = math.ceil(Position_T)
 
-    liste = ['.'] * display
+    display_list = ['.'] * display
 
     if pos_A == pos_T:
-        liste[pos_A] = colored('X', 'yellow')  
+        display_list[pos_A] = colored('X', 'yellow')  
     else:
-        liste[pos_A] = colored('A', "red")  
-        liste[pos_T] = colored('T', "green")  
+        display_list[pos_A] = colored('A', "red")  
+        display_list[pos_T] = colored('T', "green")  
 
-    print(''.join(liste))
+    print(''.join(display_list))
     time.sleep(0.5)
 
 print("-" * 156)
 time.sleep(0.4)
 cprint("X", "yellow", end="")
 time.sleep(0.4)
-print(" Signifie que la valeur entre la Tortue et Achille est extrèmement petite.")
+print(" Indicates that the distance between the Tortoise and Achilles is extremely small.")
 time.sleep(0.4)
-cprint("Achille ne parvient jamais à dépasser la Tortue …", "magenta")
+cprint("Achilles never manages to overtake the Tortoise…", "magenta")
 time.sleep(0.4)
 print("-" * 156)
 time.sleep(0.4)
-cprint("Fin de la simulation", "red")
+cprint("End of simulation", "red")
 time.sleep(0.4)
 print("-" * 156)
